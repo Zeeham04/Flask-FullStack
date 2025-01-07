084acb283563ff6b585b606a807e8800b3671644
[Zeeham04] <[zeehammad@gmail.com]>
2025-01-06 19:10:38 -0700

    made 2 pages in the base and the home along with the different pages for sign-up, login and logout

--- website/__init__.py
+++ website/__init__.py
@@ -4,5 +4,14 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'NAZEEH'
 
+    from .views import views
+    from .auth import auth
+
+    # Keep views at '/'
+    app.register_blueprint(views, url_prefix='/')
+
+    # Remove url_prefix for auth to make routes directly accessible
+    app.register_blueprint(auth)
+
     return app
 
